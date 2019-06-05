07c3a2b343bc49236141b7a01ae4e357fe731ba2
Till Brede <tb@symcon.de>
2019-06-05 14:26:10 +0200

    Initial commit

---
+++ SchereSteinPapier/module.php
@@ -0,0 +1,111 @@
+<?
+class SchereSteinPapier extends IPSModule {
+    
+    public function Create(){
+        //Never delete this line!
+        parent::Create();
+        
+        //These lines are parsed on Symcon Startup or Instance creation
+        //You cannot use variables here. Just static values.
+        $this->RegisterVariableInteger("WahlS", "Deine Wahl:", "SSP", 0);
+        $this->EnableAction("WahlS");
+		
+    }
+
+    public function Destroy(){
+        //Never delete this line!
+        parent::Destroy();
+        
+    }
+
+    public function ApplyChanges(){
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
+	
+		
+	
+	public function RequestAction($Ident, $Value) {
+		
+		$Parent = $this->InstanceID;
+		
+		switch ($Ident) {
+			case "WahlS":
+				SetValue(IPS_GetObjectIDByIdent($Ident, $Parent), $Value);
+				$WahlS = GetValue(IPS_GetObjectIDByIdent("WahlS", $Parent));
+				$WahlC = mt_rand(0, 2); 
+
+			
+
+				switch($WahlS) {
+
+				case 0:
+					switch($WahlC){
+					
+					case 0:
+						echo "Unentschieden";
+						break;
+					case 1:
+						echo "Verloren\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					case 2:
+						echo "Gewonnen\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					default:
+						echo "Computer wählte eine nicht existente Variable";
+					}
+					break;
+				case 1:
+					switch($WahlC){
+					
+					case 0:
+						echo "Gewonnen\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					case 1:
+						echo "Unentschieden";
+						break;
+					case 2:
+						echo "Verloren\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					default:
+						echo "Computer wählte eine nicht existente Variable";
+					}
+					break;
+				case 2:
+					switch($WahlC){
+					
+					case 0:
+						echo "Verloren\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					case 1:
+						echo "Gewonnen\n" . "Computer wählte " . $this->GetComputerW($WahlC);
+						break;
+					case 2:
+						echo "Unentschieden";
+						break;
+					default:
+						echo "Computer wählte eine nicht existente Variable";
+						break;
+					}
+				break;
+				}   
+		} 			
+	}	
+	
+	private function GetComputerW($Wahl) {
+		switch($Wahl){
+			case 0:
+				$Gegenstand = "Schere";
+				break;
+			case 1:
+				$Gegenstand = "Stein";
+				break;
+			case 2:
+				$Gegenstand = "Papier";
+				break;
+		}
+		return $Gegenstand;
+	}  
+
+}
+?>
